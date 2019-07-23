55d94a3eec6c86c2f6b8e496716c1fd33112ed7b
sravyadittakavi <43791983+sravyadittakavi@users.noreply.github.com>
2019-07-23 13:56:10 -0700

    Max Profit

---
+++ MaxProfit.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxProfit1
+{
+    class MaxProfit
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+            Console.WriteLine(GetMaxProfitWithSingleTransaction(prices));
+            Console.WriteLine(GetMaxProfitWithMultipleTransactions(prices));
+            Console.ReadKey();
+        }
+
+        public static int GetMaxProfitWithSingleTransaction(int[] prices)
+        {
+            int max_profit = 0, min_price = Int32.MaxValue;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] <= min_price)
+                {
+                    min_price = prices[i];
+                }
+                else
+                {
+                    if (prices[i] - min_price > max_profit)
+                        max_profit = prices[i] - min_price;
+                }
+            }
+            return max_profit;
+        }
+
+        public static int GetMaxProfitWithMultipleTransactions(int[] prices)
+        {
+            int max_profit = 0;
+            for(int i = 0; i < prices.Length-1; i++)
+            {
+                if(prices[i] < prices[i + 1])
+                {
+                    max_profit += prices[i + 1] - prices[i];
+                }
+            }
+            return max_profit;
+        }
+    }
+}
