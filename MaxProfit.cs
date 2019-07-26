cb69cd03480979ce2567a5d5554ea082b0c42ec3
sravyadittakavi <43791983+sravyadittakavi@users.noreply.github.com>
2019-07-26 09:29:22 -0700

    changes to max profit file - added with cooldown method

--- MaxProfit.cs
+++ MaxProfit.cs
@@ -13,6 +13,7 @@ namespace MaxProfit1
             int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
             Console.WriteLine(GetMaxProfitWithSingleTransaction(prices));
             Console.WriteLine(GetMaxProfitWithMultipleTransactions(prices));
+            Console.WriteLine(GetMaxProfitWithMultipleTransactionsAndCooldown(prices));
             Console.ReadKey();
         }
 
@@ -46,6 +47,23 @@ namespace MaxProfit1
                 }
             }
             return max_profit;
+        }
+
+        public static int GetMaxProfitWithMultipleTransactionsAndCooldown(int[] prices)
+        {
+            if (prices == null || prices.Length <= 1)
+                return 0;
+            int b0 = -prices[0], b1 = b0;
+            int s0 = 0, s1 = 0, s2 = 0;
+
+            for(int i = 1; i < prices.Length; i++)
+            {
+                b0 = Math.Max(b1, s2 - prices[i]);
+                s0 = Math.Max(s1, b1 + prices[i]);
+                b1 = b0; s2 = s1; s1 = s0;
+            }
+
+            return s0;
         }
     }
 }
