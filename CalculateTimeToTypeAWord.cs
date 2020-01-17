dc7e369aa8d89ce02a0c123ee34a1d2ee669efce
sravyadittakavi <43791983+sravyadittakavi@users.noreply.github.com>
2020-01-17 08:42:19 -0800

    Added 2 programs

---
+++ CalculateTimeToTypeAWord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateTimeToTypeAWord
+{
+    class CalculateTimeToTypeAWord
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateTime("pqrstuvwxyzabcdefghijklmno", "leetcode"));
+            Console.ReadLine();
+        }
+
+        private static int CalculateTime(string keyboard, string word)
+        {
+            int prevIndex = 0, result = 0, currIndex = 0;
+            foreach(var a in word)
+            {
+                currIndex = keyboard.IndexOf(a);
+                result += Math.Abs(prevIndex - currIndex);
+                prevIndex = currIndex;
+            }
+            return result;
+        }
+    }
+}
---
+++ CountSubstringsWithOneDistinctLetter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSubstringsWithOneDistinctLetter
+{
+    class CountSubstringsWithOneDistinctLetter
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountLetters("aaaaaaaaaa"));
+            Console.ReadLine();
+        }
+
+        public static int CountLetters(string S)
+        {
+            if(S==null || S.Length == 0)
+            {
+                return 0;
+            }
+            if(S.Length == 1)
+            {
+                return 1;
+            }
+            int result = 0;
+            char prevChar = S[0];
+            int repeatCount = 0;
+            foreach(var a in S)
+            {
+                char nextChar = a;
+                if(nextChar == prevChar)
+                {
+                    repeatCount++;
+                }
+                else
+                {
+                    repeatCount = 1;
+                }
+                result += repeatCount;
+                prevChar = nextChar;
+            }
+            return result;
+        }
+    }
+}
