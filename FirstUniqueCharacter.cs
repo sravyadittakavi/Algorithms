bf7ed22409c0b88419342136b122adca55044570
sravyadittakavi <43791983+sravyadittakavi@users.noreply.github.com>
2020-05-05 16:06:18 -0700

    First unique character in a string

---
+++ FirstUniqueCharacter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstUniqueCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstUniqChar("leetcode"));
+            Console.ReadKey();
+        }
+
+        public static int FirstUniqChar(string s)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c] = charCount[c] + 1;
+                }
+                else
+                {
+                    charCount.Add(c, 1);
+
+                }
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charCount[s[i]] == 1)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
