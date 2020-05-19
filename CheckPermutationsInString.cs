6537d0f235a456f2ac6ad32b5f09717619067ce6
sravyadittakavi <43791983+sravyadittakavi@users.noreply.github.com>
2020-05-19 12:50:36 -0700

    Check if permutations of one string exist in another string

---
+++ CheckPermutationsInString.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPermutationsInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckInclusion("ab", "eidbaooo"));
+            Console.ReadLine();
+        }
+
+        public static bool CheckInclusion(string s1, string s2)
+        {
+            int s1Length = s1.Length, s2Length = s2.Length;
+            if (s1Length > s2Length) return false;
+
+            int[] s1Count = new int[26];
+            int[] s2Count = new int[26];
+
+            // build reference array using string s1
+            foreach (char ch in s1.ToCharArray())
+            {
+                s1Count[(int)(ch - 'a')]++;
+            }
+
+            // sliding window on the string s2
+            for (int i = 0; i < s2Length; ++i)
+            {
+                // add one more letter 
+                // on the right side of the window
+                s2Count[(int)(s2[i] - 'a')]++;
+                // remove one letter 
+                // from the left side of the window
+                if (i >= s1Length)
+                {
+                    s2Count[(int)(s2[i - s1Length] - 'a')]--;
+                }
+                // compare array in the sliding window
+                // with the reference array
+                if (string.Join(",", s1Count) == string.Join(",", s2Count))
+                {
+                    //They are the same
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
