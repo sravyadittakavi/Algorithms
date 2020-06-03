be4c914a237ca72104216ad8a257199d56aca084
sravyadittakavi <43791983+sravyadittakavi@users.noreply.github.com>
2020-06-02 17:33:04 -0700

    Delete given node in linked list

---
+++ DeleteGivenNodeInLinkedList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// Delete node in linkedlist given access to only the node to be deleted.
+/// </summary>
+public class Solution
+{
+    public void DeleteNode(ListNode node)
+    {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
