fa46c3282774eed153b92d758377eb8703936523
sravyadittakavi <43791983+sravyadittakavi@users.noreply.github.com>
2020-06-15 13:50:57 -0700

    Search a BST

---
+++ SearchBST.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Solution
+{
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        while (root != null && val != root.val)
+            root = val < root.val ? root.left : root.right;
+        return root;
+    }
+}
